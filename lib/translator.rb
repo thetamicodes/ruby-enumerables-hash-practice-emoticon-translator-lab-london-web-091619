5f9b8c5aa0d5faac62dce63c39bafcc69eed4590
Tami Reichold <reichold.tamara@gmail.com>
2019-09-14 18:55:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,8 +6,8 @@ file_path = './lib/emoticons.yml'
 def load_library(file_path)
   hash = {:get_meaning => {}, :get_emoticon => {}}
   # file_path = './lib/emoticons.yml'
-  emoticons = YAML.load_file(file_path)
-  emoticons.each do |key, value|
+  emoticons_file = YAML.load_file(file_path)
+  emoticons_file.each do |key, value|
     hash[:get_meaning][value[1]] = key
     hash[:get_emoticon][value[0]] = value[1] 
   end
@@ -16,7 +16,7 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  # emoticons = YAML.load_file(file_path)
+  ÷emoticons_file = YAML.load_file(file_path)
 end
 
 def get_english_meaning(file_path, emoticon)
