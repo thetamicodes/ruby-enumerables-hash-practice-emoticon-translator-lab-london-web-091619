b7debf581e9d9c44f9f269d2cf3c63fd7a3728eb
Tami Reichold <reichold.tamara@gmail.com>
2019-09-14 18:45:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,11 +9,13 @@ def load_library(file_path)
     hash[:get_meaning][value[1]] = key
     hash[:get_emoticon][value[0]] = value[1] 
   end
-  hash 
+  return hash 
   binding.pry
 end
 
 def get_japanese_emoticon(file_path, emoticon)
+  
+  
   load_library.each do |meaning_hash, emoticon_hash|
   # binding.pry
   
