74212b87dca1810c6e315a768ba5cf4b7082fd31
Tami Reichold <reichold.tamara@gmail.com>
2019-09-14 19:11:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,6 @@ def load_library(file_path)
   # binding.pry
 end
 
-def get_japanese_emoticon(file_path, emoticon)
 def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
  library = load_library(file_path)
  if library["get_emoticon"].include?(english_emoticon)
